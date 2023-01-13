35daf111b191e2ab5339ee013930515250b13930
aviiciii <laavesh1@gmail.com>
2023-01-13 15:35:05 +0530

    register and login completed

--- users/models.py
+++ users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    username= None
+    username = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(db_index=True, null=True, unique=True)
     date_added = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-
-    REQUIRED_FIELDS=[]
+    role = models.CharField(max_length=50, blank=True, null=True)
+    REQUIRED_FIELDS=['username']
     USERNAME_FIELD = 'email'
 
--- users/views.py
+++ users/views.py
@@ -4,17 +4,19 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-# from .forms import CreateUserForm
+from .forms import CreateUserForm
 
+def index(request):
+    return render(request, 'users/index.html')
 
 def register(request):
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect('login')
     else:
-        # form = CreateUserForm()
-        form='jj'
+        form = CreateUserForm()
+        
         if request.method == 'POST':
-            # form = CreateUserForm(request.POST)
+            form = CreateUserForm(request.POST)
             if form.is_valid():
                 form.save()
                 user = form.cleaned_data.get('username')
@@ -25,5 +27,25 @@ def register(request):
         context = {'form':form}
         return render(request, 'users/register.html', context)
 
-def index(request):
-    return render(request, 'users/index.html')
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('professor_home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('professor_home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'users/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
