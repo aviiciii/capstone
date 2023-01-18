016f6b93752d2b883166025e2e5b05681887fd39
aviiciii <laavesh1@gmail.com>
2023-01-18 14:13:31 +0530

    working on creating students
    created mild db structure

--- professor/urls.py
+++ professor/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='professor_home'),
+    # path('profile/', views.profile, name='professor_profile'),
+    path('add/student', views.add_student, name='add_student'),
 ]
--- professor/views.py
+++ professor/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'professor/index.html')
+
+
+def add_student(request):
+    return render(request, 'professor/add_students.html')
+
